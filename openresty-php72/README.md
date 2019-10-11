32aede0b59f5db0c3e1c1d86d4732387b2ea2efb
Nopost <nopost@qq.com>
2019-10-11 21:31:28 +0800

    add openresty-php72

---
+++ openresty-php72/README.md
@@ -0,0 +1,5 @@
+基础镜像 jupincu/centos-bt:v3.0.6.9
+
+安装宝塔面板:openresty+php7.2
+
+v3.30.6.9
---
+++ openresty-php72/container-files/install/build/0027_install_php_72.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#set -ex
+cd /www/server/panel && \
+sh ./install/install_soft.sh 0 install php 7.2
+
+
